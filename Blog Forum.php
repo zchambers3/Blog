6a1c6164952292576115dc194ba84923af3d7825
James-Ryan Stampley <32416753+theartistjamesryan@users.noreply.github.com>
2018-07-03 21:11:21 -0700

    Update Blog Forum.php

--- Blog Forum.php
+++ Blog Forum.php
@@ -10,60 +10,60 @@ include("navigation.php")
 <p align="center">View the blog posts below!</p>
 </body>
 
-    <?php
-        include ("./index/footer.php");
-    ?>
+<?php
+include ("./index/footer.php");
+?>
 <style>
     <?php
         include ("./index/footer.css")
     ?>
 </style>
-
 </html>
+
 <?php
 session_start();
 
+//database connection
+include("./index/server.php");
 
-    //database connection
-    include("./index/server.php");
-
-
-$results = mysqli_query($conn, "SELECT title, textarea, date
+$results = mysqli_query($conn, "SELECT id, title, userid, date, textarea
                                       FROM blog_post");
 
-    while ($row = mysqli_fetch_array($results)) { ?>
+while ($row = mysqli_fetch_array($results)) { ?>
 
-        <table border="2" cellpadding="3">
-            <thead>
-            <tr>
-                <th>Title of Post</th>
-                <th>Username</th>
-                <th>Date of Post</th>
-                <th>Post Body</th>
-                <th colspan="2">Action</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <!--Replace everything below in td
-                so that is more Generalized-->
-                <td><?php echo $row['title']; ?></td>
-                <td><?php echo $row['Username']; ?></td>
-                <td><?php echo $row['date']; ?></td>
-                <td>Here is my Post! I love this posting blog section!</td>
-                <td>
-                    <a href="#">Edit</a>
-                </td>
-                <td>
-                    <a href="#">Delete</a>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+    <table border="2" cellpadding="3">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Username</th>
+            <th>Date of Post</th>
+            <th>Post Body</th>
+            <th colspan="2">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <!--Replace everything below in td
+            so that is more Generalized-->
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php echo $row['userid']; ?></td>
+            <td><?php echo $row['date']; ?></td>
+            <td><?php echo $row['textarea']; ?></td>
+            <td>
+                <a href='edit.php?edit=$row[id]'>Edit</a>
+            </td>
+            <td>
+                <a href='./index/delete.php?delete=$row[, title]'>Delete</a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
 
-	<?php } ?>
+<?php } ?>
 
 <style>
     <?php include("./index/Forum navigation.css")?>
 </style>
-
+<?php include("./index/footer.php"); ?>
